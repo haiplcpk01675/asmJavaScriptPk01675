a362161d19e47cb522c042c450216d85d143359a
HaiplcPK01675 <haioanh221017@gmail.com>
2020-05-06 16:42:18 +0700

    thay doi

--- formValidation.js
+++ formValidation.js
@@ -25,12 +25,49 @@ function ktraHopLe(nodeCha){
             if( phanTram > 100 ){
                 nodeError.innerHTML = tenInput + ' trong khoảng 0 đến 100%';
                 hople = false;
+                continue;
+            }
+        }
+        if(danhSachInput[i].hasAttribute('khoanggioihan')){
+            var ngay = parseInt(danhSachInput[i].value);
+            if( ngay > 8 || ngay < 2 ){
+                nodeError.innerHTML = tenInput + ' trong khoảng 2 đến 8';
+                hople = false;
+                continue;
+            }
+        }
+        if(danhSachInput[i].hasAttribute('sodt')){
+            var sdt = danhSachInput[i].value;
+            if( sdt.length != 10 ){
+                nodeError.innerHTML = tenInput + ' số điện thoại phải có 10 số.';
+                hople = false;
+                continue;
+            }
+        }
+        if(danhSachInput[i].hasAttribute('quagioihan')){
+            var phanTram = parseInt(danhSachInput[i].value);
+            if( phanTram > 100 ){
+                nodeError.innerHTML = tenInput + ' trong khoảng 0 đến 100%';
+                hople = false;
+                continue;
+            }
+        }
+        if(danhSachInput[i].hasAttribute('email')){
+            if(!ValidateEmail(danhSachInput[i].value)){
+                nodeError.innerHTML = tenInput + ' sai định dạng Email.';
+                hople = false;
             }
         }
     }
     return hople;
 }
-
+function ValidateEmail(mail){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+    {
+        return true;
+    }
+        return false;
+}
 function Onvaluechange(){
     var nodeError = event.target.nextElementSibling;
     nodeError.innerHTML = '';
