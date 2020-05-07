f1867ca5883802740b4ba01f63eca49a61a24170
HaiplcPK01675 <haioanh221017@gmail.com>
2020-05-07 17:01:47 +0700

    tha đổi ngày 7th5

--- until.js
+++ until.js
@@ -6,3 +6,23 @@ function taoID(){
     var id = Math.random().toString().substr(2,5)+"_"+String(thoiGianHienTai);
     return id;
 }
+function anDangKy(){
+    let nodeDangKy = document.getElementById('dangKy');
+    nodeDangKy.style.display = 'none';
+}
+function anDangNhap(){
+    let nodeDangNhap = document.getElementById('dangNhap');
+    nodeDangNhap.style.display = 'none';
+}
+function nodeDangNhap(){
+    let nodeDangKy = document.getElementById('dangKy');
+    let nodeDangNhap = document.getElementById('dangNhap');
+    nodeDangKy.style.display = 'none';
+    nodeDangNhap.style.display = 'flex';
+}
+function nodeDangKy(){
+    let nodeDangKy = document.getElementById('dangKy');
+    let nodeDangNhap = document.getElementById('dangNhap');
+    nodeDangNhap.style.display = 'none';
+    nodeDangKy.style.display = 'flex';    
+}
